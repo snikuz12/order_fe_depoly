899aeab9741529a06ad49ab4c7a54a833dfe9c21
snikuz12 <snikuz12@naver.com>
2024-08-23 12:26:27 +0900

    fix

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -10,7 +10,7 @@
                          </div>
                     </v-col>
                 <v-col class="text-center">
-                    <v-btn :to="{path:'/'}">shop</v-btn>
+                    <v-btn :to="{path:'/'}">shop12</v-btn>
                 </v-col>
                 <v-col class="d-flex justify-end">
                     <v-btn v-if="isLogin"  :to="{path:'/order/cart'}">장바구니({{ getTotalQuantity }})</v-btn>
